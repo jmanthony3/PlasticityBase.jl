67873ebf71c54aacfd4666ee3eacd5d5d1ad0a80
Documenter.jl <documenter@juliadocs.github.io>
2024-10-31 14:57:07 +0000

    build based on 068f2e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlasticityBase","category":"page"},{"location":"#PlasticityBase","page":"Home","title":"PlasticityBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlasticityBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlasticityBase]","category":"page"}]
+}
