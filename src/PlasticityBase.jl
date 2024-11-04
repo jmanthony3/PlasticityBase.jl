143b564488bd7437ce85ecbbf7c6d02c0ad3a606
Joby M. Anthony III <jmanthony1@liberty.edu>
2024-11-04 15:41:15 -0500

    add functions for method dispatching in user-defined material models

--- src/PlasticityBase.jl
+++ src/PlasticityBase.jl
@@ -2,12 +2,17 @@ module PlasticityBase
 
 abstract type Plasticity end
 
+function materialproperties(::Type{<:Plasticity}) end
+function materialconstants(::Type{<:Plasticity}) end
+function materialconstants_index(::Type{<:Plasticity}) end
 function record!() end
 function referenceconfiguration() end
 function solve!() end
 
-
 export Plasticity
+export materialproperties
+export materialconstants
+export materialconstants_index
 export record!
 export referenceconfiguration
 export solve!
